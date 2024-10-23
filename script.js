1d04745f11aa7ad39d031557361e2401df6e9e01
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-10-23 13:05:34 +0200

    add function for checking if symbol won he game

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
 const Game = (function () {
-  
+  function createGameboard() {
+    const gameboard = Array.from({ length: 9 }, (_, i) => 0);
+
+    return { gameboard };
+  }
+
+  function didSymbolWin(symbol) {
+    // 1, 5, 9
+    // 3, 5, 7
+    // 3 in row
+    if (
+      gameboard[0] == symbol &&
+      gameboard[4] == symbol &&
+      gameboard[8] == symbol
+    ) {
+      return true;
+    }
+
+    if (
+      gameboard[2] == symbol &&
+      gameboard[4] == symbol &&
+      gameboard[6] == symbol
+    ) {
+      return true;
+    }
+
+    let len = 0;
+    for (let val of gameboard) {
+      if (val == symbol) {
+        len += 1;
+        if (len == 3) {
+          return true;
+        }
+      } else {
+        len = 0;
+      }
+    }
+
+    return false;
+  }
 
   function createPlayer(name, symbol) {
     return { name, symbol };
   }
 
   return {
-
+    createGameboard,
     createPlayer,
+    didSymbolWin
   };
 })();
 
+console.log(Game.createGameboard().checkGameState("="));
