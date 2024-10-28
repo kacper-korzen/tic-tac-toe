83e26f6b5513517ee2ed6240e7a3402c9d8c862f
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-10-28 22:45:26 +0100

    add functionality to check who win and change background depending on it

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ const Game = (function () {
   let meatImg = "<img src='img/meat.png' alt='meat icon'>";
   const backgrounds = ['img/start-pattern.png', 'img/cats-won-pattern.png', 'img/bears-won-pattern.png'];
 
+  function checkGameState() {
+    if (winner === 'm') {
+      changeBackground(backgrounds[1]);
+      console.log("cats");
+      deleteClickEvent();
+    } else if (winner === 'b') {
+      changeBackground(backgrounds[2]);
+      console.log("bears");
+      deleteClickEvent();
+    } else if (checkIfFull()) {
+      console.log("draw");
+    }
+  }
   function createGameboardArray() {
     const gameboard = Array.from({ length: 9 }, (_, i) => 0);
 
@@ -42,7 +55,7 @@ const Game = (function () {
       gameboard[8] === symbol
     ) {
       winner = symbol;
-      return true;
+      
     }
 
     if (
@@ -51,7 +64,6 @@ const Game = (function () {
       gameboard[6] === symbol
     ) {
       winner = symbol;
-      return true;
     }
 
     let len = 0;
@@ -60,39 +72,49 @@ const Game = (function () {
         len += 1;
         if (len == 3) {
           winner = symbol;
-          return true;
         }
       } else {
         len = 0;
       }
     }
 
-    return false;
+    for (let col = 0; col < 3; col++) {
+      if (gameboard[col] === symbol && 
+          gameboard[col + 3] === symbol && 
+          gameboard[col + 6] === symbol) {
+        winner = symbol;
+      }
+  }
+
   }
 
   function createPlayer(name, symbol) {
     return { name, symbol };
   }
 
+  
+
   function createOneSquare() {
     const square = document.createElement("div");
     square.classList.add("square");
     
-    square.addEventListener("click", () => {
-      if (playingSymbol === 'b' && gameboard[square.id] === 0) {
+    const clickHandler = function()  {
+      if (playingSymbol === 'b' && gameboard[square.id] === 0 && winner === '') {
         gameboard[square.id] = 'm';
         square.innerHTML = meatImg;
         playingSymbol = 'm';
-        didSymbolWin('m');
-        checkIfFull();
-      } else if (playingSymbol === 'm' && gameboard[square.id] === 0) {
+        didSymbolWin('m'); 
+        checkGameState();
+      } else if (playingSymbol === 'm' && gameboard[square.id] === 0 && winner === '') {
         gameboard[square.id] = 'b';
         square.innerHTML = bambooImg;
         playingSymbol = 'b';
-        checkIfFull();
-        didSymbolWin('b');
+        didSymbolWin('b'); 
+        checkGameState();
       }
-    });
+    };
+
+    square.addEventListener("click", clickHandler); 
 
     return square;
   }
